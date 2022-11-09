7c20b9348c0d37d2eebcfda7b22cd8b913ba8d01
Bresly Saire <quillahuamansairebresly@gmail.com>
2022-11-09 13:34:42 -0500

    primer commit

---
+++ lib/pages/selection_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class SelectionPage extends StatelessWidget {
+  const SelectionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Example',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateTime? _selectedDate;
+
+  void _presentDatePicker() {
+    // showDatePicker is a pre-made funtion of Flutter
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2020),
+            lastDate: DateTime.now())
+        .then((pickedDate) {
+      // Check if no date is selected
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        // using state so that the UI will be rerendered when date is picked
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example'),
+      ),
+      body: Column(children: [
+        // Show the Date Picker when this button clicked
+        ElevatedButton(
+            onPressed: _presentDatePicker, child: const Text('Select Date')),
+
+        // display the selected date
+        Container(
+          padding: const EdgeInsets.all(30),
+          child: Text(
+            _selectedDate != null
+                ? _selectedDate.toString()
+                : 'No date selected!',
+            style: const TextStyle(fontSize: 30),
+          ),
+        )
+      ]),
+    );
+  }
+}
